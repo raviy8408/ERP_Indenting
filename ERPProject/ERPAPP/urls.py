db55f469ec8bcc876c5c25bdbaa06386a5c84488
raviy8408 <raviy8408@gmail.com>
2021-07-20 00:17:22 +0530

    Test table added.

--- ERPProject/ERPAPP/urls.py
+++ ERPProject/ERPAPP/urls.py
@@ -3,7 +3,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='home-page'),
+    # path('', views.test, name='home-page'),
+    path('', views.Table, name='home-page'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- ERPProject/ERPAPP/views.py
+++ ERPProject/ERPAPP/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pandas as pd
+import json
 
 # Create your views here.
+#
+# def test(request):
+#     return render(request, 'ERPAPP/Test.html')
 
-def test(request):
-    return render(request, 'ERPAPP/Test.html')
 
+def Table(request):
+    df = pd.read_csv("/Users/Ravi/PycharmProjects/ERP_Indenting/ERPProject/ERPAPP/static/ERPAPP/csv/Test.csv")
+
+    # parsing the DataFrame in json format.
+    json_records = df.reset_index().to_json(orient='records')
+    data = []
+    data = json.loads(json_records)
+    context = {'d': data}
+
+    return render(request, 'ERPAPP/Test.html', context)
